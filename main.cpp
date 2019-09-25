3c443a2ae049cbf1b2b2a891d40fc982aa086145
kmce25 <31890174+kmce25@users.noreply.github.com>
2019-09-26 00:26:45 +0100

    Attenuation working
    Light now moves independently from camera. Started attenuation again, believe it is working. Light can be moved with cube following. Attenuation can be increased/decreased. Still to fix SDL_ttf.h

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#pragma warning(disable: 4996)
+
+
+
 // MD2 animation renderer
 // This demo will load and render an animated MD2 model, an OBJ model and a skybox
 // Most of the OpenGL code for dealing with buffer objects, etc has been moved to a 
@@ -16,7 +20,7 @@
 #include <stack>
 #include "md2model.h"
 
-#include <SDL_ttf.h>
+//#include <SDL_ttf.h>
 
 using namespace std;
 
@@ -25,9 +29,13 @@ using namespace std;
 // Globals
 // Real programs don't use globals :-D
 
+float attenuationConst = 1.0f;
+float attenuationLinear = 0.02f;
+float attenuationQuadratic = 0.01f;
+
 GLuint meshIndexCount = 0;
 GLuint md2VertCount = 0;
-GLuint meshObjects[2];
+GLuint meshObjects[5];
 
 GLuint shaderProgram;
 GLuint skyboxProgram;
@@ -51,7 +59,7 @@ rt3d::lightStruct light0 = {
 	{1.0f, 1.0f, 1.0f, 1.0f}, // specular
 	{-10.0f, 10.0f, 10.0f, 1.0f}  // position
 };
-glm::vec4 lightPos(-10.0f, 10.0f, 10.0f, 1.0f); //light position
+glm::vec4 lightPos(0.0f, 2.0f, 0.0f, 1.0f); //light position
 
 rt3d::materialStruct material0 = {
 	{0.2f, 0.4f, 0.2f, 1.0f}, // ambient
@@ -70,11 +78,12 @@ rt3d::materialStruct material1 = {
 md2model tmpModel;
 int currentAnim = 0;
 
-TTF_Font * textFont;
+
+//TTF_Font * textFont;
 
 // textToTexture
-GLuint textToTexture(const char * str, GLuint textID/*, TTF_Font *font, SDL_Color colour, GLuint &w,GLuint &h */) {
-	TTF_Font *font = textFont;
+//GLuint textToTexture(const char * str, GLuint textID/*, TTF_Font *font, SDL_Color colour, GLuint &w,GLuint &h */) {
+/*	TTF_Font *font = textFont;
 	SDL_Color colour = { 255, 255, 255 };
 	SDL_Color bg = { 0, 0, 0 };
 
@@ -122,11 +131,11 @@ GLuint textToTexture(const char * str, GLuint textID/*, TTF_Font *font, SDL_Colo
 	return texture;
 }
 
-
+*/
 // Set up rendering context
-SDL_Window * setupRC(SDL_GLContext &context) {
+SDL_Window * setupRC(SDL_GLContext &context) {																
 	SDL_Window * window;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) // Initialize video
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)																	// Initialize video
         rt3d::exitFatalError("Unable to initialize SDL"); 
 	  
     // Request an OpenGL 3.0 context.
@@ -141,40 +150,40 @@ SDL_Window * setupRC(SDL_GLContext &context) {
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4); // Turn on x4 multisampling anti-aliasing (MSAA)
  
     // Create 800x600 window
-    window = SDL_CreateWindow("SDL/GLM/OpenGL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+    window = SDL_CreateWindow("SDL/GLM/OpenGL Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,				
         800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
 	if (!window) // Check window was created OK
         rt3d::exitFatalError("Unable to create window");
  
-    context = SDL_GL_CreateContext(window); // Create opengl context and attach to window
-    SDL_GL_SetSwapInterval(1); // set swap buffers to sync with monitor's vertical refresh rate
+    context = SDL_GL_CreateContext(window);																// 0.0 - Create opengl context and attach to window - Context is a data structure that collects all the info to render an image.
+    SDL_GL_SetSwapInterval(1);																			// set swap buffers to sync with monitor's vertical refresh rate
 	return window;
 }
 
 // A simple texture loading function
 // lots of room for improvement - and better error checking!
-GLuint loadBitmap(char *fname) {
+GLuint loadBitmap(char *fname) {            
 	GLuint texID;
-	glGenTextures(1, &texID); // generate texture ID
+	glGenTextures(1, &texID);																			//0.1 - Generate texture names - void glGenTextures(GLsizei n, GLuint * textures); n = amount of texture names to be generated. *textures  = Specifies an array in which the generated texture names are stored
 
 	// load file - using core SDL library
- 	SDL_Surface *tmpSurface;
+ 	SDL_Surface *tmpSurface;																			//Represent areas of "graphical" memory, memory that can be drawn to.
 	tmpSurface = SDL_LoadBMP(fname);
 	if (!tmpSurface) {
 		std::cout << "Error loading bitmap" << std::endl;
 	}
 
 	// bind texture and set parameters
-	glBindTexture(GL_TEXTURE_2D, texID);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glBindTexture(GL_TEXTURE_2D, texID);																//0.2 - Binds a texture (texID) to a texturing target. void glBindTexture(GLenum target, GLuint texture); target = textures type. If rebinding, has to be the same type
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);									//0.3 - glTexParameteri sets the parameters of the texture. The texture minifying function is used whenever the pixel being textured maps to an area greater than one texture element, 0.4 GL_LINEAR is one of the functions - Returns the weighted average of the four texture elements that are closest to the center of the pixel being textured. 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);									// GL_TEXTURE_MAG_FILTER - function is used when the pixel being textured maps to an area less than or equal to one texture element. S = x, T = y
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);								// 0.5
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-	SDL_PixelFormat *format = tmpSurface->format;
+	SDL_PixelFormat *format = tmpSurface->format;														//SDL_PixelFormat - a structure that contains pixel format information (describes the format of the pixel data stored at the pixels field of a SDL_Surface)
 	
-	GLuint externalFormat, internalFormat;
-	if (format->Amask) {
+	GLuint externalFormat, internalFormat;																//0.6 - Internal - is the format that OpenGL will use to internally store the texture data you give it.
+	if (format->Amask) {																				//Amask - Alpha mask
 		internalFormat = GL_RGBA;
 		externalFormat = (format->Rmask < format-> Bmask) ? GL_RGBA : GL_BGRA;
 	}
@@ -183,60 +192,64 @@ GLuint loadBitmap(char *fname) {
 		externalFormat = (format->Rmask < format-> Bmask) ? GL_RGB : GL_BGR;
 	}
 
-	glTexImage2D(GL_TEXTURE_2D,0,internalFormat,tmpSurface->w, tmpSurface->h, 0,
-		externalFormat, GL_UNSIGNED_BYTE, tmpSurface->pixels);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexImage2D(GL_TEXTURE_2D,0,internalFormat,tmpSurface->w, tmpSurface->h, 0,						//0.7 - Specifies a two-dimensional texture image
+		externalFormat, GL_UNSIGNED_BYTE, tmpSurface->pixels);		
+	glGenerateMipmap(GL_TEXTURE_2D);																	//generates mipmaps for a specified texture object. Mipmaps = 1/3 extra texture memory.
 
-	SDL_FreeSurface(tmpSurface); // texture loaded, free the temporary buffer
+	SDL_FreeSurface(tmpSurface);																		// texture loaded, free the temporary buffer
 	return texID;	// return value of texture ID
 }
 
 
 void init(void) {
 	
-	shaderProgram = rt3d::initShaders("phong-tex.vert","phong-tex.frag");
+	shaderProgram = rt3d::initShaders("../phong-tex.vert","../phong-tex.frag");
 	rt3d::setLight(shaderProgram, light0);
 	rt3d::setMaterial(shaderProgram, material0);
 
-	skyboxProgram = rt3d::initShaders("textured.vert","textured.frag");
+	skyboxProgram = rt3d::initShaders("../textured.vert","../textured.frag");
 
 	vector<GLfloat> verts;
 	vector<GLfloat> norms;
 	vector<GLfloat> tex_coords;
 	vector<GLuint> indices;
-	rt3d::loadObj("../../../../Resources/cube.obj", verts, norms, tex_coords, indices);
+	rt3d::loadObj("../resource/cube.obj", verts, norms, tex_coords, indices);
 	GLuint size = indices.size();
 	meshIndexCount = size;
-	textures[0] = loadBitmap("../../../../Resources/fabric.bmp");
+	textures[0] = loadBitmap("../resource/fabric.bmp");
 	meshObjects[0] = rt3d::createMesh(verts.size()/3, verts.data(), nullptr, norms.data(), tex_coords.data(), size, indices.data());
 
-	textures[1] = loadBitmap("../../../../Resources/hobgoblin2.bmp");
-	meshObjects[1] = tmpModel.ReadMD2Model("../../../../Resources/tris.MD2");
+	meshObjects[2] = rt3d::createMesh(verts.size() / 3, verts.data(), nullptr, norms.data(), tex_coords.data(), size, indices.data());
+
+
+	textures[1] = loadBitmap("../resource/hobgoblin2.bmp");
+	meshObjects[1] = tmpModel.ReadMD2Model("../resource/tris.MD2");
 	md2VertCount = tmpModel.getVertDataCount();
 
 	
 	
-	skybox[0] = loadBitmap("../../../../Resources/Town-skybox/Town_ft.bmp");
-	skybox[1] = loadBitmap("../../../../Resources/Town-skybox/Town_bk.bmp");
-	skybox[2] = loadBitmap("../../../../Resources/Town-skybox/Town_lf.bmp");
-	skybox[3] = loadBitmap("../../../../Resources/Town-skybox/Town_rt.bmp");
+	skybox[0] = loadBitmap("../resource/Town-skybox/Town_ft.bmp");
+	skybox[1] = loadBitmap("../resource/Town-skybox/Town_bk.bmp");
+	skybox[2] = loadBitmap("../resource/Town-skybox/Town_lf.bmp");
+	skybox[3] = loadBitmap("../resource/Town-skybox/Town_rt.bmp");
 	//skybox[4] = loadBitmap("Town-skybox/Town_up.bmp");
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
-
+	/*
 	// set up TrueType / SDL_ttf
 	if (TTF_Init()== -1)
 		cout << "TTF failed to initialise." << endl;
 
-	textFont = TTF_OpenFont("../../../../Resources/MavenPro-Regular.ttf", 48);
+	textFont = TTF_OpenFont("../resource/MavenPro-Regular.ttf", 48);
 	if (textFont == NULL)
 		cout << "Failed to open font." << endl;
 		
 	labels[0] = 0;//First init to 0 to avoid memory leakage if it is dynamic
 	labels[0] = textToTexture(" Hello ", labels[0]);//Actual set up of label. If dynamic, this should go in draw function
+	*/
 }
 
 glm::vec3 moveForward(glm::vec3 pos, GLfloat angle, GLfloat d) {
@@ -249,12 +262,60 @@ glm::vec3 moveRight(glm::vec3 pos, GLfloat angle, GLfloat d) {
 
 void update(void) {
 	const Uint8 *keys = SDL_GetKeyboardState(NULL);
-	if ( keys[SDL_SCANCODE_W] ) eye = moveForward(eye,r,0.1f);
-	if ( keys[SDL_SCANCODE_S] ) eye = moveForward(eye,r,-0.1f);
-	if ( keys[SDL_SCANCODE_A] ) eye = moveRight(eye,r,-0.1f);
-	if ( keys[SDL_SCANCODE_D] ) eye = moveRight(eye,r,0.1f);
+	if (keys[SDL_SCANCODE_W])
+	{		
+		eye = moveForward(eye, r, 0.1f);		
+	}
+	if (keys[SDL_SCANCODE_S])
+	{
+		eye = moveForward(eye, r, -0.1f);		
+	}
+	if (keys[SDL_SCANCODE_A])
+	{
+		eye = moveRight(eye, r, -0.1f);		
+	}
+	if (keys[SDL_SCANCODE_D])
+	{
+		eye = moveRight(eye, r, 0.1f);		
+	}
+
+	if (keys[SDL_SCANCODE_I])
+	{
+		lightPos = glm::vec4(moveForward(lightPos, 0.0f, 0.1f), 1.0f);
+	}
+
+	if (keys[SDL_SCANCODE_J])
+	{
+		lightPos = glm::vec4(moveRight(lightPos, 0.0f, -0.1f), 1.0f);
+	}
+
+	if (keys[SDL_SCANCODE_K])
+	{
+		lightPos = glm::vec4(moveForward(lightPos, 0.0f, -0.1f), 1.0f);
+	}
+
+	if (keys[SDL_SCANCODE_L])
+	{
+		lightPos = glm::vec4(moveRight(lightPos, 0.0f, 0.1f), 1.0);
+	}
+
+	if (keys[SDL_SCANCODE_U])
+	{
+		lightPos.y += 0.1;
+	}
+
+	if (keys[SDL_SCANCODE_H])
+	{
+		lightPos.y -= 0.1;
+	}
+
 	if ( keys[SDL_SCANCODE_R] ) eye.y += 0.1;
 	if ( keys[SDL_SCANCODE_F] ) eye.y -= 0.1;
+
+	if (keys[SDL_SCANCODE_Z]) light0.ambient[0] += 1.0f;
+
+
+	//cout << lightPos.x << "\n" << lightPos.y << "\n" << lightPos.z << "\n";
 
 	if ( keys[SDL_SCANCODE_COMMA] ) r -= 1.0f;
 	if ( keys[SDL_SCANCODE_PERIOD] ) r += 1.0f;
@@ -275,15 +336,28 @@ void update(void) {
 		if (++currentAnim >= 20) currentAnim = 0;
 		cout << "Current animation: " << currentAnim << endl;
 	}
-}
 
+
+	if (keys[SDL_SCANCODE_B])
+	{
+		attenuationQuadratic -= 0.01;
+		cout << attenuationQuadratic;
+	}
+
+	if (keys[SDL_SCANCODE_N])
+	{
+		attenuationQuadratic += 0.01;
+		cout << attenuationQuadratic;
+	}
+
+	
+}
 
 void draw(SDL_Window * window) {
 	// clear the screen
 	glEnable(GL_CULL_FACE);
 	glClearColor(0.5f,0.5f,0.5f,1.0f);
 	glClear(GL_COLOR_BUFFER_BIT  | GL_DEPTH_BUFFER_BIT);
-	
 
 
 	glm::mat4 projection(1.0);
@@ -349,12 +423,7 @@ void draw(SDL_Window * window) {
 
 	// back to remainder of rendering
 	glDepthMask(GL_TRUE); // make sure depth test is on
-
-	
-
-
-
-	
+		
 	glUseProgram(shaderProgram);
 
 	glm::vec4 tmp = mvStack.top()*lightPos;
@@ -365,6 +434,15 @@ void draw(SDL_Window * window) {
 
 
 	rt3d::setUniformMatrix4fv(shaderProgram, "projection", glm::value_ptr(projection));
+
+	int attenConst = glGetUniformLocation(shaderProgram, "attenuationConst");
+	glUniform1f(attenConst, attenuationConst);
+
+	int attenLinear = glGetUniformLocation(shaderProgram, "attenuationLinear");
+	glUniform1f(attenLinear, attenuationLinear);
+
+	int attenQuadratic = glGetUniformLocation(shaderProgram, "attenuationQuadratic");
+	glUniform1f(attenQuadratic, attenuationQuadratic);
 
 
 	// draw a cube for ground plane
@@ -394,23 +472,32 @@ void draw(SDL_Window * window) {
 	rt3d::drawMesh(meshObjects[1], md2VertCount, GL_TRIANGLES);
 	mvStack.pop();
 	glCullFace(GL_BACK);
+	   	 	
+	//glUseProgram(skyboxProgram);//Use the texture shader again to draw the labelled cube
+	//rt3d::setUniformMatrix4fv(skyboxProgram, "projection", glm::value_ptr(projection));
+	//// draw a cube block on top of ground plane
+	//// with text texture 
+	////Keep the texture-only shader
+	//glDepthMask(GL_FALSE); // make sure writing to update depth test is off
+	////glBindTexture(GL_TEXTURE_2D, labels[0]);
+	//mvStack.push(mvStack.top());
+	//mvStack.top() = glm::translate(mvStack.top(), glm::vec3(-1.0f, 2.0f, -7.0f));
+	//mvStack.top() = glm::scale(mvStack.top(), glm::vec3(1.0f, 1.0f, 0.0f));
+	//rt3d::setUniformMatrix4fv(skyboxProgram, "modelview", glm::value_ptr(mvStack.top()));
+	//rt3d::drawIndexedMesh(meshObjects[0], meshIndexCount, GL_TRIANGLES);
+	//mvStack.pop();
+	  
 
-
-
-	
-	glUseProgram(skyboxProgram);//Use the texture shader again to draw the labelled cube
-	rt3d::setUniformMatrix4fv(skyboxProgram, "projection", glm::value_ptr(projection));
-	// draw a cube block on top of ground plane
-	// with text texture 
-	//Keep the texture-only shader
-	glDepthMask(GL_FALSE); // make sure writing to update depth test is off
-	glBindTexture(GL_TEXTURE_2D, labels[0]);
+	//Cube for light to follow
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
 	mvStack.push(mvStack.top());
-	mvStack.top() = glm::translate(mvStack.top(), glm::vec3(-1.0f, 2.0f, -7.0f));
-	mvStack.top() = glm::scale(mvStack.top(), glm::vec3(1.0f, 1.0f, 0.0f));
-	rt3d::setUniformMatrix4fv(skyboxProgram, "modelview", glm::value_ptr(mvStack.top()));
+	mvStack.top() = glm::translate(mvStack.top(), glm::vec3(lightPos.x, lightPos.y, lightPos.z));
+	mvStack.top() = glm::scale(mvStack.top(), glm::vec3(0.5f, 0.5f, 0.5f));
+	rt3d::setUniformMatrix4fv(shaderProgram, "modelview", glm::value_ptr(mvStack.top()));
+	rt3d::setMaterial(shaderProgram, material0);
 	rt3d::drawIndexedMesh(meshObjects[0], meshIndexCount, GL_TRIANGLES);
 	mvStack.pop();
+	
 	
 	
 	// remember to use at least one pop operation per push...
--- phong-tex.vert
+++ phong-tex.vert
@@ -3,6 +3,11 @@
 // Calculates and passes on V, L, N vectors for use in fragment shader, phong2.frag
 #version 330
 
+uniform float attenuationConst;
+uniform float attenuationLinear;
+uniform float attenuationQuadratic;
+
+
 uniform mat4 modelview;
 uniform mat4 projection;
 uniform vec4 lightPosition;
@@ -17,29 +22,58 @@ out vec3 ex_L;
 in vec2 in_TexCoord;
 out vec2 ex_TexCoord;
 
-// multiply each vertex position by the MVP matrix
-// and find V, L, N vectors for the fragment shader
+out float ex_attenuation;
+out float ex_dist;
+
+																									// multiply each vertex position by the MVP matrix and find V, L, N vectors for the fragment shader
 void main(void) {
 
-	// vertex into eye coordinates
-	vec4 vertexPosition = modelview * vec4(in_Position,1.0);
+	
+	vec4 vertexPosition = modelview * vec4(in_Position,1.0);										// vertex into eye coordinates
 
-	// Find V - in eye coordinates, eye is at (0,0,0)
-	ex_V = normalize(-vertexPosition).xyz;
+	//vec4 mylightPosition = modelview * vec4(lightPosition);
 
-	// surface normal in eye coordinates
-	// taking the rotation part of the modelview matrix to generate the normal matrix
-	// (if scaling is includes, should use transpose inverse modelview matrix!)
-	// this is somewhat wasteful in compute time and should really be part of the cpu program,
-	// giving an additional uniform input
-	mat3 normalmatrix = transpose(inverse(mat3(modelview)));
+	
+	ex_V = normalize(-vertexPosition).xyz;															// Find V - in eye coordinates, eye is at (0,0,0)
+
+
+	mat3 normalmatrix = transpose(inverse(mat3(modelview)));										// surface normal in eye coordinates taking the rotation part of the modelview matrix to generate the normal matrix
+																									//(if scaling is includes, should use transpose inverse modelview matrix!) this is somewhat wasteful in compute time and should really be part of the cpu program,
+																									// giving an additional uniform input
 	ex_N = normalize(normalmatrix * in_Normal);
 
-	// L - to light source from vertex
-	ex_L = normalize(lightPosition.xyz - vertexPosition.xyz);
+	
+	ex_L = normalize(lightPosition.xyz - vertexPosition.xyz);										// L - to light source from vertex
 
 	ex_TexCoord = in_TexCoord;
 
     gl_Position = projection * vertexPosition;
 
+	float att_distance = distance(vertexPosition, lightPosition);
+
+	ex_attenuation = (1.0 / (attenuationConst + attenuationLinear * att_distance + attenuationQuadratic * att_distance * att_distance));
+
+	//att_distance;//
+	
 }
+
+//attenuation =  1/ constant attenuation factor + (linear attenuation factor * distance) + (quadratic attenuation factor * (distance * distance)
+// distance = distance between light source and surface being shaded. 
+// kc = 1, kl = 0, kq = 0
+//kc = constant attenuation factor
+// kl = linear attenuation factor 
+//kq = quadtratic attenuation factor.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
